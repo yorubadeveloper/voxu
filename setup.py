28accc9e3df59bd6fbf9e24818052d9431255c48
yorubadeveloper <bukunmiadewale3@gmail.com>
2024-03-22 10:55:07 +0100

    Updated the setup.py file workflow update

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='voxu',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     install_requires=[
         'sqlalchemy',
         'flask',
         'psycopg2-binary'
     ],
+    keywords=['Python', 'Flask', 'Logging', 'SQLAlchemy', 'PostgreSQL'],
+    url='https://github.com/yorubadeveloper/voxu',
+    author='Yoruba Developer',
 )
